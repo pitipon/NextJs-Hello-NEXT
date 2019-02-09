b64f6781c0fa8965831277ece2adcffed8b7a71f
Pitipon Pluemworasawat <pitipon@gmail.com>
2019-02-09 21:13:32 +0800

    post page

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import Layout from '../components/Layout.js'
+
+export default () => (
+    <Layout>
+       <p>About</p>
+    </Layout>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-const Index = () => (
-    <div>
-        <p>Hello Next</p>
-    </div>
+import Layout from '../components/Layout.js';
+import Link from 'next/link';
+
+const PostLink = (props) => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
 )
 
-export default Index;
+export default () => (
+    <Layout>
+        <h1>My Blog</h1>
+        <ul>
+            <PostLink title="Hello Next.js"/>
+            <PostLink title="Hello React"/>
+            <PostLink title="Hello JS"/>
+        </ul>
+    </Layout>
+)
